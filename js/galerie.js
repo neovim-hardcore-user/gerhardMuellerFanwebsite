7247cf09f1255600135ce0a41ad970b0cda8693e
NicoDevStuff <88544750+NicoDevStuff@users.noreply.github.com>
2024-05-29 15:50:01 +0200

    Galerie Prototyp

---
+++ js/galerie.js
@@ -0,0 +1,26 @@
+console.log("balls")
+
+document.addEventListener("DOMContentLoaded", function() {
+    const imageContainer = document.getElementById('images');
+
+    const images = [];
+
+	for (let i = 1; i < 16; ++i) {
+		images.push("../images/galerie/" + i + ".jpg")
+	}
+
+	console.log(images);
+
+    function loadImages(paths) {
+        paths.forEach(path => {
+            const img = document.createElement('img');
+            img.src = path;
+            img.alt = "Image";
+			img.className = "image"
+            imageContainer.appendChild(img);
+        });
+    }
+
+    loadImages(images);
+});
+
