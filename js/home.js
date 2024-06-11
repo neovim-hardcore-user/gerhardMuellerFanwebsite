a58c327ea94970af7db44b61e3e538f32891c4f3
neovim-hardcore-user <113374980+neovim-hardcore-user@users.noreply.github.com>
2024-06-11 16:55:15 +0200

    disclaimer

--- js/home.js
+++ js/home.js
@@ -1,8 +1,18 @@
+setTimeout(() => {
+	const disclaimer = document.getElementById("disclaimer");
+
+	disclaimer.classList.add("active");
+	setTimeout(() => {
+		disclaimer.classList.add("visible");
+	 }, 10);
+
+}, 2000);
+
 function close_popup() {
 	const disclaimer = document.getElementById("disclaimer");
 	disclaimer.classList.remove("visible");
 	setTimeout(() => {
         disclaimer.classList.remove("active");
 		console.log("tf");
-    }, 200);
+    }, 400);
 }
